a62b56ea236e013dad74e389c537e1603b9b94ae
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-07-22 23:03:21 +0400

    End Terre00

---
+++ terre00.js
@@ -0,0 +1,10 @@
+
+
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+let str = "" ; 
+for (let i = 0; i < 26; i++) {
+    str += alphabet[i];
+}
+
+console.log(str);
