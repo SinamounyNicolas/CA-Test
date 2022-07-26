f543d956ddfe17885e76b7222f661dfc36ac5ff7
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-07-26 15:28:11 +0400

    Add length function in loop

--- terre00.js
+++ terre00.js
@@ -3,7 +3,7 @@
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 let str = "" ; 
-for (let i = 0; i < 26; i++) {
+for (let i = 0; i < alphabet.length ; i++) {
     str += alphabet[i];
 }
 
