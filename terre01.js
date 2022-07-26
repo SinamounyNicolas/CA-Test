a760999cf82b20251a5288a9b221ca911bab503e
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-07-26 15:32:36 +0400

    Test FilePath and FileName

--- terre01.js
+++ terre01.js
@@ -1,1 +1,9 @@
-console.log("test");
+/*
+const path = require ('path');
+
+let fileName = path.extname('/Users/nicolas/Desktop/CA/terre01.js');
+console.log(fileName);
+*/
+
+const filePath = process.argv[1];
+console.log(filePath);
