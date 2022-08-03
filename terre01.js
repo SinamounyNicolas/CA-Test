9b76fca8848c15c8d15989dbf23023c623774549
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-08-03 11:27:27 +0400

    end of the creation of the program which displays the name of the file

--- terre01.js
+++ terre01.js
@@ -1,9 +1,4 @@
-/*
-const path = require ('path');
-
-let fileName = path.extname('/Users/nicolas/Desktop/CA/terre01.js');
-console.log(fileName);
-*/
 
 const filePath = process.argv[1];
-console.log(filePath);
+const programName = filePath.split("/").pop();
+console.log(programName);
