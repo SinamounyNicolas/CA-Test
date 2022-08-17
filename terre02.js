5602bdcbae6568c1f9e1bf9619bdf0e3679cf129
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-08-17 14:27:15 +0400

    End of script

--- terre02.js
+++ terre02.js
@@ -1,11 +1,4 @@
 
-// function callArgs () {
-//     const args = process.argv.slice(2)
-//     console.log(args);
-// }
-
-// callArgs()
-
-for(let i = 0; i < process.argv.length; ++i) {
-    console.log(`${process.argv[i]}`);
-}
+    for(let i = 2; i < process.argv.length; ++i) {
+        console.log(`${process.argv[i]}`);
+    }
