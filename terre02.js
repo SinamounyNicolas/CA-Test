4489fd5d2b192311629f584a5d746635ab55975e
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-08-17 11:29:16 +0400

    argument display test

--- terre02.js
+++ terre02.js
@@ -1,3 +1,11 @@
 
-let args = process.argv.slice(2)
-console.log(args);
+// function callArgs () {
+//     const args = process.argv.slice(2)
+//     console.log(args);
+// }
+
+// callArgs()
+
+for(let i = 0; i < process.argv.length; ++i) {
+    console.log(`${process.argv[i]}`);
+}
