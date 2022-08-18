42f0e79d232c5969a338a45020bae03c3ad39e2f
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-08-18 14:54:01 +0400

    Add comment

--- terre01.js
+++ terre01.js
@@ -1,4 +1,4 @@
 
-const filePath = process.argv[1];
-const programName = filePath.split("/").pop();
+const filePath = process.argv[1]; // retrieve the path and the class according to the requested index
+const programName = filePath.split("/").pop();  // split allows you to specify a separator, pop retrieve the last information from the table and display it
 console.log(programName);
