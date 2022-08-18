45553c89f4c2280440753b8048874abbd423dd8c
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-08-18 15:02:18 +0400

    Add comment

--- terre02.js
+++ terre02.js
@@ -1,4 +1,4 @@
 
-    for(let i = 2; i < process.argv.length; ++i) {
-        console.log(`${process.argv[i]}`);
+    for(let i = 2; i < process.argv.length; ++i) { // initialization of a 2 to retrieve only user arguments
+        console.log(`${process.argv[i]}`); // the loop causes the arguments to display inline
     }
