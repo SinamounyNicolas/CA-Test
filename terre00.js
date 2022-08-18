e190fb8a5d9381dbaa25e2351f811d376ccd048a
Nicolas Sinamouny <nicolas.sinamouny@gmail.com>
2022-08-18 14:47:28 +0400

    Add comment

--- terre00.js
+++ terre00.js
@@ -1,10 +1,9 @@
 
-
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-let str = "" ; 
-for (let i = 0; i < alphabet.length ; i++) {
-    str += alphabet[i];
+let str = "" ; // initialization of the variable str in empty string to be able to insert the elements to concatenate 
+for (let i = 0; i < alphabet.length ; i++) { 
+    str += alphabet[i];                     // concatenation of $alphabet with str
 }
 
 console.log(str);
